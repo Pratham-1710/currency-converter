ffe73427954264016cd56fcdc053c1bb42fc81cf
Pratham Namdev <prathnam1710@gmail.com>
2026-03-23 00:16:17 +0530

    frontend complete

---
+++ src/hooks/useCurrencyinfo.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://open.er-api.com/v6/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => {
+        console.log("API:", res);
+        setData(res.rates);
+      })
+      .catch((err) => console.log(err));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
+// import { useEffect,useState } from "react";
+
+// function useCurrencyInfo(currency){
+//     const [data,setData] = useState({});
+//     useEffect(()=>{
+//         fetch(`https://api.frankfurter.app/latest?from=${currency}`)
+//           .then((res) => res.json())
+//           .then((res) => setData(res.rates));
+//         console.log(data);
+//     },[currency])
+//     console.log(data);
+//     return data
+// }
+
+// export default useCurrencyInfo;
